9cb61a334c23281eca223811bf7d49d255ab276b
RianUmar <adriansyah.umar07@gmail.com>
2025-11-23 21:00:55 +0800

    Perubahan Comment

--- test_app.py
+++ test_app.py
@@ -1,3 +1,11 @@
+#Tugas_RPL
+
+#1. Ardiansyah N. Umar          F55124015
+#2. Anggra Giovano Tapadongko   F55124023
+#3. Chatrin Aurelia             F55124019
+#4. Anisa Aulia Febrina         F55124005
+#5. Atalya Victoria             F55124028
+
 from app import tambah, kurang
 
 def test_tambah():
